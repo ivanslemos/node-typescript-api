e46b4b07566b003e9c14bd32536f8daabc2fd499
Lusca <luscasimoni@gmail.com>
2020-10-04 20:02:06 -0300

    Finalized the test and API client mockup

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,14 +6,15 @@ import stormGlassNormalized3HoursFixture from '../../../test/fixtures/stormglass
 jest.mock('axios')
 
 describe('StormGlass client ', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
   it('should return the normalize forecast from the StormGlass service', async () => {
     const lat = -33.792726
     const lng = 151.289824
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
-    const stormGlass = new StormGlass(axios)
-    const response = stormGlass.fetchPoints(lat, lng)
+    const stormGlass = new StormGlass(mockedAxios)
+    const response = await stormGlass.fetchPoints(lat, lng)
     expect(response).toEqual(stormGlassNormalized3HoursFixture)
   })
 })
