43579c5895dfed499853cf8c4a8b6b992a855944
Lusca <luscasimoni@gmail.com>
2020-10-07 17:30:52 -0300

    Dynamic configs with node-config

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
 import { InternalError } from '@src/util/errors/internal-error'
 import { AxiosStatic } from 'axios'
+import config, { IConfig } from 'config'
 
 export interface StormGlassPointSource {
   [key: string]: number
@@ -45,6 +46,8 @@ export class StormGlassResponseError extends InternalError {
   }
 }
 
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
+
 export class StormGlass {
   readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
   readonly stormGlassAPISource = 'noaa'
@@ -53,10 +56,10 @@ export class StormGlass {
 
   public async fetchPoints (lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
-      const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
+      const response = await this.request.get<StormGlassForecastResponse>(`${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
         {
           headers: {
-            Authorization: 'fake-token'
+            Authorization: stormGlassResourceConfig.get('apiToken')
           }
         })
       return this.normalizeResponse(response.data)
